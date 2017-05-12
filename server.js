81168b4f787177d208b106bdf9f798ebe26ebbe4
jackyan63@gmail.com <jackyan63@gmail.com>
2017-05-11 21:56:47 -0400

    Update middleware for localhost

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require('express');
+const path = require('path');
 
 //Create App
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(PORT, error => {
-  error
-  ? console.error(error)
-  : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
-});
-
+app.use(function (req, res, next){
+  if(req.headers['x-forwared-proto'] == 'https') {
+    res.redirect('http://' + req.hostname + req.url)
+  }else{
+    next();
+  }
+})
 app.use(express.static('public'));
+app.listen(3000,function(){
+  console.log('Express server is up on port 3000.')
+});
