4e8beafb37e2e2a7f728047c9aeff57019ba057f
jackyan63@gmail.com <jackyan63@gmail.com>
2017-05-28 18:10:37 -0400

    Add Custom Styles

--- app/app.jsx
+++ app/app.jsx
@@ -7,15 +7,18 @@ var About = require('About');
 var Examples = require('Examples');
 
 // Load foundation
-require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
+// App css
+require('style!css!applicationStyles')
+
 ReactDOM.render(
-  <Router history = {hashHistory}>
-    <Route path = "/" component = {Main}>
-      <Route path = "about" component = {About}/>
-      <Route path = "examples" component = {Examples}/>
-      <IndexRoute components = {Weather}/>
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
