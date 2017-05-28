3c7c061cda7c08e7cb4dfa462da245106a817978
jackyan63@gmail.com <jackyan63@gmail.com>
2017-05-28 18:56:30 -0400

    Update Nav Search

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -33,7 +33,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
